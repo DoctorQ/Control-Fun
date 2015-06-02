445b3d42cd2a46c6cb94f5b3da59b3725b2de689
58 <doctor19891203>
2015-06-02 22:25:48 +0800

    Initial Commit

---
+++ Control Fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Control Fun
+//
+//  Created by 58 on 15/6/2.
+//  Copyright (c) 2015年 doctorq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
