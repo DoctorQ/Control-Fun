f1b5d92c2b3274f7024941a9fc15a88adc09b0b3
58 <doctor19891203>
2015-06-23 22:35:31 +0800

    DEV:UP

--- Control Fun/ViewController.h
+++ Control Fun/ViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIActionSheetDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
